e880170ec5e7130761aa08f93a6ef0d62adb4be2
Harry Youd <harry@harryyoud.co.uk>
2017-05-15 08:25:57 +0100

    Remove Gello from PRODUCT_PACKAGES
    Change-Id: Ib21379422df740b0ebc4bd28efadbbbdd5f3015c

--- device.mk
+++ device.mk
@@ -81,10 +81,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
-# Browser
-PRODUCT_PACKAGES += \
-    Gello
-
 # Camera
 PRODUCT_PACKAGES += \
     Snap
