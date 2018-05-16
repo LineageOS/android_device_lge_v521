7262834c25c36b9cde23e60fa9364468c577e6b6
Rashed Abdel-Tawab <rashed@linux.com>
2018-05-16 12:18:22 -0700

    v521: Build blutooth HALs
    Change-Id: Ic8f78cd9cc4e11f713ba46c38601cf46e0b4326c

--- device.mk
+++ device.mk
@@ -86,6 +86,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
