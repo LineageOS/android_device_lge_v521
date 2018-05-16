7df5ae5d424cbf2538c1e9b4a179af94b1bdc843
Rashed Abdel-Tawab <rashed@linux.com>
2018-05-15 20:35:03 -0700

    v521: Set kernel image name
    Change-Id: I3ffe9db0f67c954ea13f94596345b48a585ad332

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,11 +60,11 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET     := 0x01000000
+BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_KERNEL_SOURCE := kernel/lge/msm8952
-TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_CONFIG := lineageos_v521_defconfig
 ifeq ($(FORCE_64_BIT),true)
 TARGET_KERNEL_ARCH := arm64
